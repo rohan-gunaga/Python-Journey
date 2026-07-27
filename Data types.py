c7f7c2bfd2eeaae6e6214029e36a0fa015f8121d
Rohan <rohangunaga953@gmail.com>
2026-07-27 17:08:39 +0530

    Add data types.py to git

---
+++ Data types.py
@@ -0,0 +1,14 @@
+age = 22 
+is_student = False 
+weight = 69.5 
+
+print(type(weight))
+print(weight)
+print(type(is_student))
+
+is_student = "yes"
+
+print(type(is_student))
+
+age_float = float(age)
+print(age_float)
