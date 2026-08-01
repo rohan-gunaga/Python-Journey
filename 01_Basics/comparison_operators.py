d7e5d9bcee2a408b5be6da3fe95d3f5dee2d573e
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-01 23:22:24 +0530

    Added comparison operators practice and updated assignment operators

---
+++ 01_Basics/comparison_operators.py
@@ -0,0 +1,26 @@
+a = 20
+b = 40 
+
+print(a < b)
+print(a != b)
+print(a == b)
+print(a > b)
+print(a <= b)
+
+# problem 1
+
+first_Number = int(input(" Enter first number "))
+second_Number = int(input(" Enter second number "))
+
+print("Is first_number greater than second?")
+print(first_Number > second_Number)
+
+# problem 2
+
+salary = int(input("Enter salary: "))
+
+print("High_salary")
+print(salary > 50000)
+
+
+
