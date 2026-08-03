d1706863d0ce13a73851db66ec4ad3d4bf40bc51
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-03 21:07:41 +0530

    membership operators.py update

---
+++ 01_Basics/membership_operators.py.py
@@ -0,0 +1,22 @@
+name = "Rohan"
+name_2 = "Rohan Ravi Gunaga"
+
+print(("v" in name_2))
+
+print(("R" in name) or ("R" in name_2))
+
+
+# problem 1:
+students = ["Rohan", "Ravi", "Gunaga", "Suresh", "Kumar"]
+
+is_Rohan_present = "mohan" in students
+
+print(is_Rohan_present)
+
+# problem 2:
+
+password = "python@123"
+
+has_special_symbol = "@" in password
+
+print(has_special_symbol)
