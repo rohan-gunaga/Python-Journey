377c632cbfa5e640cfefbcef2b67312b6609a992
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-04 22:25:32 +0530

    bitwise_operators.py update

---
+++ 01_Basics/bitwise_operators.py
@@ -0,0 +1,55 @@
+a = 5
+b = 3
+
+# bitwise AND
+print(a & b)
+
+# bitwise OR
+print(a | b)
+
+# bitwise XOR
+print(a ^ b)
+
+# bitwise NOT
+print(~a)
+
+# Left shift
+print(a << 1)
+
+# Right shift
+print(a >> 1)
+
+# problem 1:
+
+a = 12
+b = 5
+
+# find bitwise AND
+
+and_result = a & b
+
+print(and_result)
+
+
+# problem 2:
+
+a = 25
+b = 18
+
+# find bitwise AND,OR, XOR
+
+result = a & b
+print("bitwise AND:" , result)
+
+result = a | b
+print("bitwise OR:" , result)
+
+result = a ^ b 
+print("bitwise XOR:" , result)
+
+# problem 3:
+
+number = 7
+
+doubled = number << 1
+print("doubled value of 7 is:" , doubled)
