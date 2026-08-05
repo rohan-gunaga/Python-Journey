e7801cbb1068751ff297ef92c06c5f52558c4604
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-05 22:56:56 +0530

    lists.py update

---
+++ 01_Basics/lists.py
@@ -0,0 +1,46 @@
+names = ["Rohan", "Satwik", "Rohit", "Tushar", "Bhavana"]
+
+print(names[0])
+print(names[-1])
+print(names[2])
+print(names[-3])
+
+
+# problem 1:
+
+colors = ["red" , "blue" , "green" , "yellow" , "black"]
+
+print(colors[1])
+
+
+# problem 2:
+animals = ["cat" , "dog" , "lion"]
+
+# change "cat" to "tiger"
+animals[0] = "tiger"
+
+# print updated string
+print(animals)
+
+
+# problem 3:
+
+numbers = [5 , 10 , 15]
+
+# insert 7 at index 1
+
+numbers .insert(1, 7)
+
+# print the list 
+print(numbers)
+
+
+# problem 4:
+
+marks = [75 , 88 , 91 , 67 , 80]
+
+# find the highest marks of the list 
+highest_marks = max(marks)
+
+# print the highest marks 
+print(highest_marks)
