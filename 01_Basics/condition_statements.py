ab4e8eb85c6f115b370bda2d287d1a7e33e64df6
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-12 21:36:48 +0530

    condition_statement update

---
+++ 01_Basics/condition_statements.py
@@ -0,0 +1,19 @@
+# If statement 
+
+x = 21
+
+if x%2==0:
+    print("x is even")
+else:
+    print("x is odd")
+
+
+# if-elif-else
+signal = "yellow"
+
+if signal =="red":
+    print("STOP")
+elif signal =="yellow":
+    print("READY")
+else:
+    print("GO")
