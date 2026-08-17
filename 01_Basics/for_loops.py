ffa3634ea12c3f136f132ebaf9c0b3c6e899970c
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-17 21:46:21 +0530

    for_loops.py pushing

---
+++ 01_Basics/for_loops.py
@@ -0,0 +1,29 @@
+# for_loops
+
+for i in range(1, 6):
+    print(i)
+
+
+# problem 1:
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
+# problem 2:
+
+total = 0
+
+for i in range(1,11):
+    total = total + i
+
+print(total)
+
+# problem 3:
+
+students = ["Rohan", "Rahul", "Anjali", "Kiran"]
+
+for student in students:
+    print(student)
