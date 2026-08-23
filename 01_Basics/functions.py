d20fc215effb7a07dfccaf66ddbe62804c51e754
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-23 18:13:52 +0530

    functions.py practice

--- 01_Basics/functions.py
+++ 01_Basics/functions.py
@@ -72,3 +72,43 @@ def check_even(number):
 
 print(check_even(10))
 print(check_even(7))
+
+# problem 8:
+
+def find_max(a, b):
+    if a > b :
+        return a
+    else:
+        return b
+
+print(find_max(10, 25))
+print(find_max(50, 20))
+
+# problem 9:
+
+numbers = [10, 20, 30, 40]
+
+def sum_list(numbers):
+    total = 0
+
+    for number in numbers:
+        total = total + number
+
+    return total 
+
+print(sum_list(numbers))
+
+# problem 10:
+
+
+def count_even(numbers):
+    count = 0
+    for number in numbers:
+        if number % 2 == 0:
+            count = count + 1
+    return count
+    
+numbers = [2, 5, 8, 11, 14, 17, 20]
+
+print(count_even(numbers))
+
