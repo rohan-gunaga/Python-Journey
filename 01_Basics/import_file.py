f358d2a89d883157a525999da0e07f4f38c86e59
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-08-26 19:46:12 +0530

    Add calculator and module examples

---
+++ 01_Basics/import_file.py
@@ -0,0 +1,4 @@
+import calculator
+
+print(calculator.add(10, 20))
+print(calculator.multiply(5, 4))
---
+++ 01_Basics/modules.py
@@ -0,0 +1,16 @@
+# problem 1:
+
+import math
+
+num = int(input("enter the number: "))
+
+print("square root:", math.sqrt(num))
+print("square:", num ** 2)
+
+# problem 2:
+
+from math import sqrt
+
+number = int(input("enter the number: "))
+
+print("square root",sqrt(number))
