f33e5b0821559b250461aa08205791b1a2802250
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-09-08 19:16:04 +0530

    python_mastery.py practice completed

--- 03_Practice/python_mastery.py
+++ 03_Practice/python_mastery.py
@@ -682,6 +682,30 @@ class Car:
         self.engine.start()
         print("car started")
 
-
 car1 = Car()
 car1.start_car()
+
+# problem 51:
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+
+class Cat(Animal):
+    def speak(self):
+        print("Cat says meow")
+
+dog = Dog()
+cat = Cat()
+
+dog.speak()
+cat.speak()
